5185297e2b3175ba090956644730a4a27cee29f3
jhenrique09 <jhsv09@gmail.com>
2019-12-22 08:10:44 +0000

    raphael: Build customized version of ANX Camera

--- havoc_raphael.mk
+++ havoc_raphael.mk
@@ -21,3 +21,6 @@ PRODUCT_NAME := havoc_raphael
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
